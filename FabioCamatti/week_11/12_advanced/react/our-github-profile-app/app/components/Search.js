2e6ca34c0027078ccbb102efb98ef630e8254d29
fbi1714 <fcamatti@ucs.br>
2016-07-06 16:07:41 +1000

    Saving....

--- FabioCamatti/week_11/12_advanced/react/our-github-profile-app/app/components/Search.js
+++ FabioCamatti/week_11/12_advanced/react/our-github-profile-app/app/components/Search.js
@@ -2,12 +2,38 @@
 import React, { Component } from 'react';
 
 class Search extends Component {
+  constructor(){
+    super();
+    this.state = {
+      search: ''
+    };
+    this.updateSearch = this.updateSearch.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+  updateSearch(e){
+    this.setState({
+      search: e.currentTarget.value
+    });
+  }
+  handleSubmit(e){
+    var searchQuery = this.state.search;
+    console.log( searchQuery );
+  }
   render(){
     return (
       <div>
-        <input type="text" name="search" placeholder="Username " />
+        <input
+            type="text"
+            name="search"
+            value={this.state.search}
+            onChange={this.updateSearch}
+            placeholder="Username " />
         <br />
-        <input type="submit" className="button-primary" value="Search" />
+        <input
+            type="submit"
+            className="button-primary"
+            onClick={this.handleSubmit}
+            value={`Search for ${this.state.search}`} />
       </div>
     );
   }
