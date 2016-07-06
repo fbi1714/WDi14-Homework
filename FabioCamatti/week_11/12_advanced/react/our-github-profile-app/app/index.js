0bfc0b818116c4b6f20e5eca172471a2c209d53a
fbi1714 <fcamatti@ucs.br>
2016-07-06 15:08:16 +1000

    Saving....

--- FabioCamatti/week_11/12_advanced/react/our-github-profile-app/app/index.js
+++ FabioCamatti/week_11/12_advanced/react/our-github-profile-app/app/index.js
@@ -8,43 +8,79 @@ const USER_DATA = {
   name: "fbi"
 };
 
-class ProfilePic extends Component {
-  render() {
-    return ( <img src="" /> );
+const ProfilePic =(props) => {
+  return ( <img src={props.imageURL} style={{width: "100px"}} />)
+}
+const ProfileName =(props) => {
+  return ( <h3><em>{props.name}</em></h3> );
+}
+const ProfileLink =(props) => {
+  return (
+    <div>
+      <a href={'https://www.github.com/' + props.username} target="_blank">
+        {props.username}
+      </a>
+    </div>
+  );
+}
+
+class Paragraph extends Component {
+  render(){
+    console.log(this);
+    return ( <p>Some text goes here</p>);
   }
 }
 
-class ProfileName extends Component {
-  render() {
-    return ( <h3>Name</h3> );
-  }
-}
-
-class ProfileLink extends Component {
-  render() {
+const Avatar =(props) => {
     return (
       <div>
-        <a href="#">
-          Username
-        </a>
+        <ProfilePic imageURL={props.user.imageURL} />
+        <ProfileName name={props.user.name} />
+        <ProfileLink username={props.user.username} />
+        <Paragraph>Some text goes here</Paragraph>
+
       </div>
     );
-  }
 }
 
-class Avatar extends Component {
-  render() {
-    return (
-      <div>
-        <ProfilePic />
-        <ProfileName />
-        <ProfileLink />
-      </div>
-    );
-  }
-}
+// class ProfilePic extends Component {
+//   render() {
+//     console.log( this.props );
+//     return ( <img src={this.props.imageUrl} /> );
+//   }
+// }
 
-ReactDOM.render(<Avatar />, document.getElementById("app"));
+// class ProfileName extends Component {
+//   render() {
+//     return ( <h3>{this.props.name}</h3> );
+//   }
+// }
+//
+// class ProfileLink extends Component {
+//   render() {
+//     return (
+//       <div>
+//         <a href={'https://www.github.com/' + this.props.username}>
+//           {this.props.username}
+//         </a>
+//       </div>
+//     );
+//   }
+// }
+
+// class Avatar extends Component {
+//   render() {
+//     return (
+//       <div>
+//         <ProfilePic imageURL={this.props.user.imageURL} />
+//         <ProfileName name={this.props.user.name} />
+//         <ProfileLink username={this.props.user.username} />
+//       </div>
+//     );
+//   }
+// }
+
+ReactDOM.render(<Avatar user={USER_DATA} />, document.getElementById("app"));
 
 // class HelloWorld extends Component{
 //   render(){
