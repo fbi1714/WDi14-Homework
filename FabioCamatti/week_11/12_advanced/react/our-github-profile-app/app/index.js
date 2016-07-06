41935815d65d0b59e9d2942a60c58dfb2b2da748
fbi1714 <fcamatti@ucs.br>
2016-07-06 14:44:11 +1000

    Saving GitHub App, first steps

--- FabioCamatti/week_11/12_advanced/react/our-github-profile-app/app/index.js
+++ FabioCamatti/week_11/12_advanced/react/our-github-profile-app/app/index.js
@@ -1,3 +1,58 @@
 /*jshint esversion: 6 */
-const IMPORTANT=true
-console.log(IMPORTANT);
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+const USER_DATA = {
+  imageURL: "https://avatars.githubusercontent.com/u/9123421?v=3",
+  username: "fbi1714",
+  name: "fbi"
+};
+
+class ProfilePic extends Component {
+  render() {
+    return ( <img src="" /> );
+  }
+}
+
+class ProfileName extends Component {
+  render() {
+    return ( <h3>Name</h3> );
+  }
+}
+
+class ProfileLink extends Component {
+  render() {
+    return (
+      <div>
+        <a href="#">
+          Username
+        </a>
+      </div>
+    );
+  }
+}
+
+class Avatar extends Component {
+  render() {
+    return (
+      <div>
+        <ProfilePic />
+        <ProfileName />
+        <ProfileLink />
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<Avatar />, document.getElementById("app"));
+
+// class HelloWorld extends Component{
+//   render(){
+//     return (
+//       <div> Hello World </div>
+//     );
+//   }
+// }
+//
+//
+// ReactDOM.render(<HelloWorld />, document.getElementById("app"));
--- FabioCamatti/week_11/12_advanced/react/our-github-profile-app/webpack.config.js
+++ FabioCamatti/week_11/12_advanced/react/our-github-profile-app/webpack.config.js
@@ -8,23 +8,17 @@ const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
 });
 
 module.exports = {
-  entry: [
-    './app/index.js'
-  ],
+  entry: ['./app/index.js'],
   output: {
     path: __dirname + "/dist",
     filename: "index_bundle.js"
   },
   module: {
-    loaders: [
-      {
-        test: /\.jsx?$/,
-        exclude: /node_modules/,
-        loader: "babel-loader"
-      }
-    ]
+    loaders: [{
+      test: /\.jsx?$/, exclude: /node_modules/, loader: "babel-loader"
+    }]
   },
   plugins: [
-    HtmlWebpackPlugin
+    HtmlWebpackPluginConfig
   ]
 };
