6331bc5d7fc630e5e047597d0acef793d4abe658
fbi1714 <fcamatti@ucs.br>
2016-07-07 12:18:50 +1000

    Saving.....

--- FabioCamatti/week_11/12_advanced/react/our-github-profile-app/app/components/Main.js
+++ FabioCamatti/week_11/12_advanced/react/our-github-profile-app/app/components/Main.js
@@ -1,11 +1,18 @@
-/*jshint esversion: 6 */
 import React, { Component } from 'react';
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
 
-class Main extends  Component {
-  render(){
+console.log(ReactCSSTransitionGroup);
+
+class Main extends Component {
+  render() {
     return (
-      <div className="container" style={{ textAlign: "center"}}>
-        {this.props.children}
+      <div className="container" style={{ textAlign: "center" }}>
+        <ReactCSSTransitionGroup
+          transitionName="appear"
+          transitionEnterTimeout={500}
+          transitionLeaveTimeout={500}>
+          { React.cloneElement(this.props.children, { key: this.props.location.pathname}) }
+        </ReactCSSTransitionGroup>
       </div>
     );
   }
--- FabioCamatti/week_11/12_advanced/react/our-github-profile-app/app/components/UserRepositories.js
+++ FabioCamatti/week_11/12_advanced/react/our-github-profile-app/app/components/UserRepositories.js
@@ -6,16 +6,23 @@ class UserRepositories extends Component{
       if(!this.props.repos){
         return ( <div>Loading...</div> );
       }
-
+      //html_url, name, id
       let repos = this.props.repos.map((repo) => {
         console.log(repo);
+        return (
+          <li key={repo.id} >
+            <a href={repo.html_url} target="_blank">
+            {repo.name}
+            </a>
+          </li>
+        );
       });
 
       return(
       <div>
         <h5>Repositories</h5>
         <ul>
-          
+          {repos}
         </ul>
       </div>
     );
