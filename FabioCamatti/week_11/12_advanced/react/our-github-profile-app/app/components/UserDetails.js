28083f6835ad7fd30621aa1824b06e7811879048
fbi1714 <fcamatti@ucs.br>
2016-07-07 11:03:01 +1000

    Saving... this project

--- FabioCamatti/week_11/12_advanced/react/our-github-profile-app/app/components/UserDetails.js
+++ FabioCamatti/week_11/12_advanced/react/our-github-profile-app/app/components/UserDetails.js
@@ -1,7 +1,11 @@
 /*jshint esversion: 6 */
 import React, { Component, PropTypes } from 'react';
+// Doesn't need to specify the file extension, e.g., JS at the end.
+import GithubHelpers from '../utils/GithubHelpers';
 
-class UserDetails extends Component {
+console.log(GithubHelpers);
+
+class UserDetailsContainer extends Component {
   render(){
     console.log( this.props );
     return (
@@ -12,9 +16,9 @@ class UserDetails extends Component {
   }
 };
 
-UserDetails.contextTypes = {
+UserDetailsContainer.contextTypes = {
   router: PropTypes.object.isRequired
 };
 
 
-export default UserDetails;
+export default UserDetailsContainer;
