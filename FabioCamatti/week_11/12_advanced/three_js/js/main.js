24f0b683a97b3767036c487c1bd0556d34d5941a
fbi1714 <fcamatti@ucs.br>
2016-07-08 11:34:53 +1000

    Saving....

--- FabioCamatti/week_11/12_advanced/three_js/js/main.js
+++ FabioCamatti/week_11/12_advanced/three_js/js/main.js
@@ -1,24 +1,49 @@
 var app = app || {};
 
-app.init = function () {
-  app.scene = new THREE.Scene();
+app.init = function() {
+    app.scene = new THREE.Scene();
+    app.width = window.innerWidth;
+    app.height = window.innerHeight;
 
-  app.camera = new THREE.PerspectiveCamera;(45, app.width / app.height, 0.1, 1000 );
+    app.camera = new THREE.PerspectiveCamera(45, app.width / app.height, 0.1, 1000);
 
-  console.log("App started");
-
-  app.renderer = new THREE.WebGLRenderer();
-  app.width = window.innerWidth;
-  app.height = window.innerHeight;
-
-
-  app.renderer.setSize( app.width, app.height );
-  app.renderer.setClearColor( 0xECEFF1 ); "#ECEFF1"
+    app.camera.position.x = -30;
+    app.camera.position.y = 40;
+    app.camera.position.z = 30;
+    app.camera.lookAt(app.scene.position);
 
 
 
-  var target = document.getElementById("output");
-  target.appendChild( app.renderer.domElement );
+    console.log("App started");
+
+    app.renderer = new THREE.WebGLRenderer();
+
+
+
+    app.renderer.setSize(app.width, app.height);
+    app.renderer.setClearColor(0xECEFF1);
+    "#ECEFF1"
+
+    app.axes = new THREE.AxisHelper(40);
+    app.scene.add(app.axes);
+
+    var planeGeometry = new THREE.PlaneGeometry(60, 20);
+    var planeMaterial = new THREE.MeshBasicMaterial({
+        color: 0xCDF8DC
+    });
+
+    app.plane = new THREE.Mesh(planeGeometry, planeMaterial);
+
+    app.plane.position.x = 10;
+    app.plane.position.y = -2;
+    app.plane.position.z = 0;
+
+    app.scene.add(app.plane);
+
+
+    var target = document.getElementById("output");
+    target.appendChild(app.renderer.domElement);
+    app.renderer.render(app.scene, app.camera);
 
 };
 
