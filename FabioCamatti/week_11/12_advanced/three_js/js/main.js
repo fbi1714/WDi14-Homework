6fb01b6d19f0095c831faaeed5d013c206ccea12
fbi1714 <fcamatti@ucs.br>
2016-07-08 11:14:25 +1000

    .

--- FabioCamatti/week_11/12_advanced/three_js/js/main.js
+++ FabioCamatti/week_11/12_advanced/three_js/js/main.js
@@ -1,1 +1,26 @@
+var app = app || {};
 
+app.init = function () {
+  app.scene = new THREE.Scene();
+
+  app.camera = new THREE.PerspectiveCamera;(45, app.width / app.height, 0.1, 1000 );
+
+  console.log("App started");
+
+  app.renderer = new THREE.WebGLRenderer();
+  app.width = window.innerWidth;
+  app.height = window.innerHeight;
+
+
+  app.renderer.setSize( app.width, app.height );
+  app.renderer.setClearColor( 0xECEFF1 ); "#ECEFF1"
+
+
+
+  var target = document.getElementById("output");
+  target.appendChild( app.renderer.domElement );
+
+};
+
+// IMPORTANT LINE OF CODE TO LEARN AND KEEP IN MIND
+window.onload = app.init;
